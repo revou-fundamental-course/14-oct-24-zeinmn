9882b7c83a64a7b9c000768ef8dee73a604fe1ef
zeinmn <uzeinwelah@gmail.com>
2024-10-23 17:53:53 +0700

    input nama user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// ini javascript
+// mengambil input nama
+function replaceName() {
+  let name = prompt("Hai dengan siapakah ini?", "");
+  document.getElementById("name").innerHTML = name;
+}
+replaceName();
